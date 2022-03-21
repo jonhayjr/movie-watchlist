81f1c0df93c509df7e978d8fcbc22bcca1fca90a
jonhayjr <jonhayjr@gmail.com>
2022-03-21 08:02:13 -0700

    Added skeleton for movie card

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 import config from "./config.js";
 
+const movieContainer = document.getElementById('movie-container');
+let movieData = [];
+
+const getMovies = async(search = 'Batman') => {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${config.apiKey}&type=movie&s=${search}`)
+    const data = await response.json();
+    movieData = await data.Search;
+    console.log(movieData)
+}
+
+const renderHTML = arr => {
+
+}
+
+getMovies()
